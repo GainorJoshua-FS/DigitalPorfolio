03184d3ee01d76073a42fbe50d88ccfee9faaa15
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2022-03-16 16:50:40 -0400

    Browser Router set up and header started

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import Header from './components/header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
 
 function App() {
   return (
     <main style={styles.main}>
-      <Header />
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Header />
+        <h2>Hello World</h2>
+        {/* <Routes>
+
+        </Routes> */}
+      </BrowserRouter>
     </main>
   );
 }
--- portfolio/src/components/header.js
+++ portfolio/src/components/header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Logo2 from '../images/Logo.png'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <header>
-            
+        <header style={styles.header}>
+            <Link to="/">
+                <img src={Logo2} alt="JG Logo" />
+            </Link>
+            <h1>Header</h1>
         </header>
     )
 }
@@ -12,6 +17,8 @@ export default Header
 
 const styles ={
     header:{
-        backgroundColor: "#2c3160"
+        backgroundColor: "#2c3160",
+        padding: "20px 0",
+        display: "flex"
     }
 }
