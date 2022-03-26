50e53194313a213e48ff64d6f60ccc466c546e00
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2022-03-26 17:53:51 -0400

    Kanban information done

--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -7,37 +7,42 @@ const home = "<- Home"
 
 function Kanban() {
     return (
-        <section >
+        <section style={styles.section} >
                 <Link style={styles.link} to="/"> {home} </Link>
-                <h2>Coming Soon!</h2>
+                {/* <h2>Coming Soon!</h2>
                 <p>
                     This page is still under development
-                </p>
-            {/* <section style={styles.div}>
-                <img style={styles.col} src={collection} alt="A bookshelf full of board games" />
-                <img style={styles.col} src={homescreen} alt="A color pallet"/>
-            </section> */}
-            {/* <section>
+                </p> */}
+            <section style={styles.div}>
+                <img style={styles.col} src={KanHome} alt="A Kanban Board Website's home page" />
+                <img style={styles.col} src={KanNew} alt="A modal window for adding a new task to a kanban board"/>
+            </section>
+            <section>
                 <article style={styles.article}>
                     <h2>The Idea</h2>
-                    <p style={styles.p}>I had always been a fan of games ever since I could remember. However, as I got older, I learned that board games were better at bringing people together to enjoy each other's company. Starting around my late high school years, I started collecting board games and hosting game nights at my house with my friends. One issue I started to come across was picking out a game to play and trying to list off possible games to choose from. As my collection grew, it became harder and harder to list out every option for the group. That’s where my idea for Game Night Debut came from. Now, users can keep their collection online to share to others to make it easier to view and choose a game.</p>
+                    <p style={styles.p}>
+                        The idea for this project was rather simple, create a kanban board using an API in order to get used to pulling information from an API. Another idea for this website was to create my own button that will allow the user to switch from light and dark mode. For this project, it was my first time ever pulling from an API and my first time programming something to change the theme of the website.
+                    </p>
                 </article>
                 <article style={styles.article}>
                     <h2>The Plan</h2>
-                    <p style={styles.p}>I wanted to make an app that had one simple purpose of keeping your board game collection all in one place. To do this, I started to come up with a color pallet to use for the web app. Since the topic was on board games, I wanted the color scheme to be somewhat old/classic and a little bit of retro. As for fonts, I wanted to go with something easy to read, but also something a little fancy for the title of the site. Once the colors and fonts had been picked, it was time to move onto the wireframes.
-                        <br/><br/>
-                        The wireframes started with just grayed out blocks that would show the placements of different images and text. Once I planned out the layouts of the first few screens, I began adding color, text, and images. Of course, it didn't stop there as I had to make tweaks and edits to the layouts as I populated the wireframes. Once I had a full wireframe of the app that I was proud of, I started with the code.
+                    <p style={styles.p}>
+                        The reason behind this project was to have a simple way to keep track of the tasks I needed to complete and the tasks I had coming up. The other reason behind this project was simply that it was a achool project for one of my courses during my time at Full Sail University. One of the most important part of this assignment was to have semantic HTML and to use as little divs as possible.
                     </p>
                 </article>
                 <article style={styles.article}>
                     <h2>Behind the Scenes</h2>
-                    <p style={styles.p}>Game Night Debut is a ReactJs and NodeJs based web application that’s programmed primarily for mobile use. As far as the games go, the images and the information on the games comes from an API called Board Game Atlas. The home page is sorted by the top-rated games at the time and the search page allows you to search for any board game and board game expansions. The backend consists of a Postgres database that keeps track of the plans on your account.</p>
+                    <p style={styles.p}>
+                        My kanban board uses an API from Gitch called Kanban API Server. This api stores three lists of tasks that get pulled into the code when you call to fetch them. After that, I made sure they were laid out within proper HTML tags and styled them using SCSS. I used both pulgins and mixins to make the site reactive and to change colors of certain aspects. This site also allows you to input new tasks into whichever list you choose, pushing that information into the API.
+                    </p>
                 </article>
                 <article style={styles.article}>
                     <h2>Whats Coming Next</h2>
-                    <p style={styles.p}>As of right now, Game Night Debut has the option of keeping a collection of your games and creating a plan that has a date, time, and address to keep track of when your upcoming game nights are. In the future, I plan on creating a login system and an invite system that will let you send your plans to others so that they can keep track of when and where to meet up. I also plan on putting the collection into the database that's associated to the user's account.</p>
+                    <p style={styles.p}>
+                        What's next for this project is a simple delete function to delete tasks in any list the user would need to delete them from. I also wish to impliment an edit option so the user can fix any spelling mistakes or change any due dates.
+                    </p>
                 </article>
-            </section> */}
+            </section>
         </section>
     )
 }
@@ -71,7 +76,8 @@ const styles ={
     },
     link:{
         color: "#e15c25",
-        width: "100%"
+        width: "100%",
+        fontWeight: "bolder"
     },
     p:{
         fontSize: "17px"
