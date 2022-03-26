41c0d793f30ddd73e9a9d336045292ab793d2f9b
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2022-03-26 18:03:30 -0400

    Some grammer fixes

--- src/components/Kanban.js
+++ src/components/Kanban.js
@@ -27,19 +27,19 @@ function Kanban() {
                 <article style={styles.article}>
                     <h2>The Plan</h2>
                     <p style={styles.p}>
-                        The reason behind this project was to have a simple way to keep track of the tasks I needed to complete and the tasks I had coming up. The other reason behind this project was simply that it was a achool project for one of my courses during my time at Full Sail University. One of the most important part of this assignment was to have semantic HTML and to use as little divs as possible.
+                        The reason behind this project was to have a simple way to keep track of the tasks I needed to complete and the tasks I had coming up. The other reason behind this project was simply that it was a school project for one of my courses during my time at Full Sail University. One of the most important part of this assignment was to have semantic HTML and to use as little divs as possible. So, to start, I researched which container tags to use in which situations. Once I felt comfortable, I made sure to keep it as semantic as possible so all of the information goes to where it needs. Next was the theme button. After a little bit of researched, I decided to brute-force it until it worked and then started from there to clean it up and make it run more efficiently.
                     </p>
                 </article>
                 <article style={styles.article}>
                     <h2>Behind the Scenes</h2>
                     <p style={styles.p}>
-                        My kanban board uses an API from Gitch called Kanban API Server. This api stores three lists of tasks that get pulled into the code when you call to fetch them. After that, I made sure they were laid out within proper HTML tags and styled them using SCSS. I used both pulgins and mixins to make the site reactive and to change colors of certain aspects. This site also allows you to input new tasks into whichever list you choose, pushing that information into the API.
+                        My kanban board uses an API from Glitch called Kanban API Server. This api stores three lists of tasks that get pulled into the code when you call to fetch them. After that, I made sure they were laid out within proper HTML tags and styled them using SCSS. I used both plugins and mix-ins to make the site reactive and to change colors of certain aspects. This site also allows you to input new tasks into whichever list you choose, pushing that information into the API.
                     </p>
                 </article>
                 <article style={styles.article}>
                     <h2>Whats Coming Next</h2>
                     <p style={styles.p}>
-                        What's next for this project is a simple delete function to delete tasks in any list the user would need to delete them from. I also wish to impliment an edit option so the user can fix any spelling mistakes or change any due dates.
+                        project is a simple delete function to delete tasks in any list the user would need to delete them from. I also wish to implement an edit option so the user can fix any spelling mistakes or change any due dates.
                     </p>
                 </article>
             </section>
